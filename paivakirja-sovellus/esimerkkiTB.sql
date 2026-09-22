4ed2f3766a1b37593d5fa0a0d5c6f158df8910ea
Sadbrew <132399464+Sadbrew@users.noreply.github.com>
2026-09-22 12:54:10 +0300

    +Esimerkki taulukko tietokantaan

---
+++ paivakirja-sovellus/esimerkkiTB.sql
@@ -0,0 +1,11 @@
+INSERT INTO DiaryEntries (EntryDate, Title, Content) VALUES
+('2026-09-15 08:20:00', 'Aamuinen kävely', 'Kävin aamulla lyhyellä kävelyllä. Sää oli aurinkoinen ja mieliala nousi heti.'),
+('2026-09-16 12:45:00', 'Koulupäivä', 'Tänään oli pitkä koulupäivä. Opiskelin C#:ia ja tietokantoja.'),
+('2026-09-17 19:10:00', 'Ilta kotona', 'Katsoin dokumentin ja söin pasta. Rauhallinen ilta.'),
+('2026-09-18 10:00:00', 'Projekti etenee', 'Sain päiväkirjasovellukseen luokan DiaryEntry valmiiksi. Tuntuu hyvältä edistyä.'),
+('2026-09-19 16:30:00', 'Kahvitauko', 'Kävin kahvilla kaverin kanssa. Puhuimme opiskelusta ja tulevaisuuden suunnitelmista.'),
+('2026-09-20 14:30:00', 'Ensimmäinen merkintä', 'Tämä on testisisältöä.'),
+('2026-09-21 09:15:00', 'Toinen päivä', 'Tänään opiskelin SQL:ää.'),
+('2026-09-21 18:45:00', 'Ilta', 'Päiväkirjasovellus etenee hyvin.'),
+('2026-09-22 07:50:00', 'Aikainen aamu', 'Heräsin aikaisin ja tein aamupalaa. Päivä alkoi rauhallisesti.'),
+('2026-09-22 21:15:00', 'Päivän yhteenveto', 'Tänään opin miten indeksi toimii tietokannassa. Hyödyllistä tietoa tulevaisuutta varten.');
